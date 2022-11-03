d3bb46c665b321cf25a4bcf058ba24d47ff8c8b4
이병인 <0820b@naver.com>
2022-11-04 04:41:18 +0900

    Add files via upload

--- 소스.cpp
+++ 소스.cpp
@@ -22,7 +22,76 @@
 #define MAP_X 3
 #define MAP_Y 3
 
-int GAME_STATUS = 1;
+#define STATE_MENU 1
+#define STATE_KEYSETTINGS 2
+#define STATE_PLAYGAME -1
+#define STATE_QUITGAME 0
+
+class Actor {
+private:
+public:
+    virtual void mvUp() = 0;
+    virtual void mvDown() = 0;
+    virtual void mvLeft() = 0;
+    virtual void mvRight() = 0;
+    virtual void interact() = 0;
+    virtual void Print() = 0;
+    virtual void SetCursor(int n) { return; }
+};
+class Command {
+public:
+    virtual void execute(Actor& actor) = 0;
+};
+
+
+class InputHandler {             //user modify key setting
+private:
+    Command* buttonW_;
+    Command* buttonA_;
+    Command* buttonS_;
+    Command* buttonD_;
+    Command* buttonESC_;
+    Command* buttonSPACE_;
+    Command* buttonNothing_;
+    char input;
+    char upKey = 'w';
+    char downKey = 's';
+    char leftKey = 'a';
+    char rightKey = 'd';
+    char interactKey = 32;
+    char escKey = 27;
+
+    int isPressed(char c) {
+        if (input == c)
+            return 1;
+        return 0;
+    }
+public:
+    InputHandler();
+    void SetUpKey(char key);
+    void SetDownKey(char key);
+    void SetLeftKey(char key);
+    void SetRightKey(char key);
+    void SetInteractKey(char key);
+    void setKeys();
+
+    Command*handleInput();
+};
+
+InputHandler input;
+
+// ================================================
+class GameStatus {
+private:
+    int StatusInfo[10];
+    int GAME_STATUS;
+public:
+    GameStatus():GAME_STATUS(STATE_PLAYGAME){}
+    int Get() { return GAME_STATUS; }
+    void Set(int n) { GAME_STATUS = n; }
+    void operator*= (int n) { GAME_STATUS *= n; }
+};
+GameStatus GAME_STATUS;
 
 /*struct position{
   int x,y;
@@ -35,6 +104,7 @@ void InitScreen() {
 #endif
 #ifdef WINDOW
     system("cls");
+    printf("\033[1;1H");
 #endif
 }
 
@@ -43,6 +113,7 @@ class Position {
 private:
     T x, y, temp;
     T BottomX, BottomY, TopX, TopY;
+
 public:
     Position(T X, T Y, T BX, T TX, T BY, T TY) :x(X), y(Y), BottomX(BX), BottomY(BY), TopX(TX), TopY(TY) {}
     T GetX() { return x; }
@@ -96,45 +167,149 @@ private:
         printf("\033[2J");
         printf("\033[1;1H");
     }
-    const int EscLines;
+    int limits;
+    int setting;
+    const int MenuLines;
+    const int KeyLines;
     const char* UIEscContents[3];
+    const char* UIKeys[5];
+
+    char keys[5];
+    char keysTemp[5];
+    int idx;
 public:
-    UI() :EscLines(3) {
+    UI() :MenuLines(3),KeyLines(5),limits(0),setting(0),idx(0) {
         UIEscContents[0] = "Resume";
         UIEscContents[1] = "Settings";
         UIEscContents[2] = "Quit";
+
+        UIKeys[0] = "↑ :";
+        UIKeys[1] = "↓ :";
+        UIKeys[2] = "← :";
+        UIKeys[3] = "→ :";
+        UIKeys[4] = "Interact :";
+
+        keysTemp[0] = keys[0]='w';
+        keysTemp[1] = keys[1] ='s';
+        keysTemp[2] = keys[2] ='a';
+        keysTemp[3] = keys[3] ='d';
+        keysTemp[4] = keys[4] =SPACE;
+
     }
-    int GetEscLines() { return EscLines; }
+    int RangeCheck(int cursor){
+        if (setting == 1) { //key edit mode
+            return 0;
+        }
+        if (cursor >= 0 && cursor < limits&&setting!=2)
+            return 1;
+        if (setting == 2 && cursor >= 0 && cursor<2) {
+            return 1;
+        }
+        return 0;
+    }
+    int GetEscLines() { return MenuLines; }
+    int GetSetting() {
+        return setting;
+    }
     void Print(int cursor) {
-        switch (GAME_STATUS) {
-        case -1:
-            for (int i = 0; i < EscLines; i++) {
+        switch (GAME_STATUS.Get()) {
+
+        case STATE_MENU:                //menu page
+            limits = MenuLines;
+
+            for (int i = 0; i < MenuLines; i++) {
                 if (cursor == i)
                     printf("\033[45m");
-
                 printf("%s\n", UIEscContents[i]);
                 printf("\033[0m");
             }
             break;
-        case -2:
-            printf("not yet :p\n");
+
+        case STATE_KEYSETTINGS:         //key setting page
+            limits = KeyLines;
+
+            for (int i = 0; i < KeyLines; i++) {
+                if (cursor == i&&setting==0) {
+                    printf("\033[45m");
+                }
+                printf("%s", UIKeys[i]);
+                printf("\033[0m");
+
+                if (setting == 1&&cursor==i) {
+                    printf("\033[45m");
+                }
+                printf("%c\n",keysTemp[i] );
+                printf("\033[0m");
+            }
+
+            if (setting == 2) {
+                printf("===fix===\n");
+                if(cursor==0)
+                    printf("\033[45m");
+                printf("yes");
+                printf("\033[0m");
+                printf("    ");
+                if (cursor == 1)
+                    printf("\033[45m");
+                printf("no");
+                printf("\033[0m");
+            }
             break;
+
         }
     }
     void Select(int cursor) {
-        if (GAME_STATUS == -1) {            //press ESC once menu
+
+        if (GAME_STATUS.Get() == STATE_MENU) {            //now you on the menu
             switch (cursor) {
             case 0:
-                GAME_STATUS = 1;
+                GAME_STATUS.Set(STATE_PLAYGAME);
                 break;
             case 1:
-                GAME_STATUS = -2;
+                GAME_STATUS.Set(STATE_KEYSETTINGS);
                 break;
             case 2:
-                GAME_STATUS = 0;
+                GAME_STATUS.Set(STATE_QUITGAME);
                 break;
             }
+        }else if (GAME_STATUS.Get() == STATE_KEYSETTINGS) {      //now you on the key settings
+           if (setting == 0) {
+                setting = 1;
+            }
         }
+
+        if (setting == 1 && GAME_STATUS.Get() == STATE_KEYSETTINGS) {   //키 변경 중
+            char temp = keys[cursor];
+            idx = cursor;
+            while (temp != SPACE) {
+                keysTemp[cursor] = temp;
+                InitScreen();
+                Print(cursor);
+                temp = _getch();
+            }
+            setting = 2;
+        }else if (setting == 2) { //cursor값이 cursorX가 넘어와야 함
+            Print(cursor);
+            if (cursor == 0) {//설정
+                keys[idx] = keysTemp[idx];
+                input.SetUpKey(keys[0]);
+                input.SetDownKey(keys[1]);
+                input.SetLeftKey(keys[2]);
+                input.SetRightKey(keys[3]);
+                input.SetInteractKey(keys[4]);
+            }
+            if (cursor == 1) {//설정 취소
+                keysTemp[idx] = keys[idx];
+            }
+            setting = 0;
+        }
+    }
+    int STATECANCHANGE() {
+        if (setting != 2)
+            return 1;
+    }
+    void BreakAll() {
+        setting = 0;
     }
 };
 
@@ -193,7 +368,7 @@ public:
 
 Map map;
 
-class Actor {
+/*class Actor {
 private:
 public:
     virtual void mvUp() = 0;
@@ -202,42 +377,55 @@ public:
     virtual void mvRight() = 0;
     virtual void interact() = 0;
     virtual void Print() = 0;
-    virtual void InitCursor() { return; }
-};
+    virtual void SetCursor(int n) { return; }
+};*/
 //===========Actor's Childs==========
 class UIActor :public Actor {
 private:
     int cursor;
-    int cursorLimit_ESC;
+    int cursorX;
 public:
-    UIActor(UI& ui) :cursor(0) {
-        cursorLimit_ESC = ui.GetEscLines();
+    UIActor() :cursor(0),cursorX(0) {
     }
     //==for Command class==
     void mvUp() {
-        cursor = RangeChecker<int>(cursor, -1, 0, cursorLimit_ESC);
+        //cursor = RangeChecker<int>(cursor, -1, 0, cursorLimit_ESC);
+        if(ui.RangeCheck(cursor-1)) //it can move?
+            cursor--;
     }
     void mvDown() {
-        cursor = RangeChecker<int>(cursor, 1, 0, cursorLimit_ESC - 1);
+        //cursor = RangeChecker<int>(cursor, 1, 0, cursorLimit_ESC - 1);
+        if (ui.RangeCheck(cursor + 1))  //it can move?
+            cursor++;
     }
     void mvLeft() {
+        if (ui.RangeCheck(cursorX - 1))
+            cursorX--;
         return;
     }
     void mvRight() {
+        if (ui.RangeCheck(cursorX +1))
+            cursorX++;
         return;
     }
-    void interact() {
-        ui.Select(cursor);
+    void interact() {           //=========================방금 변경한 것
+        if (ui.GetSetting() != 2)
+            ui.Select(cursor);
+        else if (ui.GetSetting() == 2)
+            ui.Select(cursorX);
         return;
     }
     //==================
-    void InitCursor() {
-        cursor = 0;
+    void SetCursor(int n) {
+        cursor = n;
     }
     void Print() {
         InitScreen();
         //printf("%d\n",cursor);
-        ui.Print(cursor);
+        if (ui.GetSetting() != 2)
+            ui.Print(cursor);
+        else if (ui.GetSetting() == 2)
+            ui.Print(cursorX);
     }
 };
 
@@ -274,10 +462,10 @@ public:
 };
 //=======================================
 
-class Command {
+/*class Command {
 public:
     virtual void execute(Actor& actor) = 0;
-};
+};*/
 
 //=============Command's Childs========
 /*class Command_UI{
@@ -311,7 +499,7 @@ public:
 class quitCommand :public Command {
 public:
     virtual void execute(Actor& actor) {
-        GAME_STATUS = 0;
+        GAME_STATUS.Set(0);
     }
 };
 class EmptyCommand :public Command {
@@ -324,18 +512,23 @@ public:
 class ESCCommand :public Command {
 public:
     virtual void execute(Actor& actor) {
-        if (GAME_STATUS == -1) actor.InitCursor();         //open ESC menu. init cursor pos
-        if (GAME_STATUS < -1) {                     //current page: esc-Setting. this line must under "... InitCursor()" line
-            GAME_STATUS = -1;
+        if (ui.STATECANCHANGE()) {
+            if (GAME_STATUS.Get() == STATE_MENU) actor.SetCursor(0);  //when ESC menu opend, init cursor pos
+
+            if (GAME_STATUS.Get() == STATE_KEYSETTINGS) {                     //current page: keySetting. this line must under "... InitCursor()" line
+                GAME_STATUS.Set(STATE_MENU);
+                ui.BreakAll();
+                actor.SetCursor(1);
+            }
+            else
+                GAME_STATUS *= -1;
         }
-        else
-            GAME_STATUS *= -1;
     }
 };
 //==========================================
 
 
-class InputHandler {             //user modify key setting
+/*class InputHandler {             //user modify key setting
 private:
     Command* buttonW_;
     Command* buttonA_;
@@ -345,6 +538,13 @@ private:
     Command* buttonSPACE_;
     Command* buttonNothing_;
     char input;
+
+    char upKey='w';
+    char downKey = 's';
+    char leftKey = 'a';
+    char rightKey = 'd';
+    char interactKey = 32;
+    char escKey = 27;
 
     int isPressed(char c) {
         if (input == c)
@@ -362,39 +562,90 @@ public:
 
         buttonESC_ = new(ESCCommand);
     }
+    void SetUpKey(char key) { upKey = key; }
+    void SetDownKey(char key) { downKey = key; }
+    void SetLeftKey(char key) { leftKey = key; }
+    void SetRightKey(char key) { rightKey = key; }
+    void SetInteractKey(char key) { interactKey = key; }
+    void setKeys() {
+    }
 
     Command* handleInput() {
 #ifdef LINUX
         input = getch();
 #endif
-#ifndef LINUX
 #ifdef WINDOW
         input = _getch();
 #endif
-#endif
+
         fflush(stdin);
 
-        if (isPressed(W)) return buttonW_;
-        if (isPressed(A)) return buttonA_;
-        if (isPressed(S)) return buttonS_;
-        if (isPressed(D)) return buttonD_;
-        if (isPressed(ESC)) return buttonESC_;
-        if (isPressed(SPACE)) return buttonSPACE_;
+        if (isPressed(upKey)) return buttonW_;
+        if (isPressed(leftKey)) return buttonA_;
+        if (isPressed(downKey)) return buttonS_;
+        if (isPressed(rightKey)) return buttonD_;
+        if (isPressed(escKey)) return buttonESC_;
+        if (isPressed(interactKey)) return buttonSPACE_;
         return buttonNothing_;
     }
 };
-
 InputHandler input;
+*/
+//=================================================================추가부
+
+
+InputHandler::InputHandler() {
+        buttonW_ = new(UpCommand);
+        buttonA_ = new(LeftCommand);
+        buttonS_ = new(DownCommand);
+        buttonD_ = new(RightCommand);
+        buttonSPACE_ = new(InteractCommand);
+        buttonNothing_ = new(EmptyCommand);
+
+        buttonESC_ = new(ESCCommand);
+    }
+    void InputHandler::SetUpKey(char key) { upKey = key; }
+    void InputHandler::SetDownKey(char key) { downKey = key; }
+    void InputHandler::SetLeftKey(char key) { leftKey = key; }
+    void InputHandler::SetRightKey(char key) { rightKey = key; }
+    void InputHandler::SetInteractKey(char key) { interactKey = key; }
+    void InputHandler::setKeys() {
+    }
+
+    Command* InputHandler::handleInput() {
+#ifdef LINUX
+        input = getch();
+#endif
+#ifdef WINDOW
+        input = _getch();
+#endif
+
+        fflush(stdin);
+
+        if (isPressed(upKey)) return buttonW_;
+        if (isPressed(leftKey)) return buttonA_;
+        if (isPressed(downKey)) return buttonS_;
+        if (isPressed(rightKey)) return buttonD_;
+        if (isPressed(escKey)) return buttonESC_;
+        if (isPressed(interactKey)) return buttonSPACE_;
+        return buttonNothing_;
+    }
+// ====================================================================여까지
+
+
+
 
 int main(void) {
     GameActor player;
-    UIActor cursor(ui);
-    while (GAME_STATUS) {
-        if (GAME_STATUS > 0) {
+    UIActor cursor;
+    printf("이동키: W,A,S,D\n상호작용: SPACE\n설정: ESC\n\n\n시작하려면 상호작용 키를 누르세요");
+    do {} while (_getch() != SPACE);
+    while (GAME_STATUS.Get()) {
+        if (GAME_STATUS.Get()==STATE_PLAYGAME) {
             player.Print();
             input.handleInput()->execute(player);
         }
-        if (GAME_STATUS < 0) {
+        if (GAME_STATUS.Get()>=STATE_MENU) {
             cursor.Print();
             input.handleInput()->execute(cursor);
 
